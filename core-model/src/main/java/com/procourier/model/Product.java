1af0f30321e49fb02f518a9ba9bb7b7ef8b1ab00
Maria <filipmariafm@gmail.com>
2019-03-21 13:39:17 +0200

    second commit

--- core-model/src/main/java/com/procourier/model/Product.java
+++ core-model/src/main/java/com/procourier/model/Product.java
@@ -1,4 +1,22 @@
-package com.procourier;
+package com.procourier.model;
+
+import java.util.Objects;
 
 public class Product {
+    private Long price;
+    private String name;
+    private String description;
+    private Long weight;
+
+
+    public Product(Long price, String name, String description, Long weight) {
+        this.price = Objects.requireNonNull(price);
+        this.name = Objects.requireNonNull(name);
+        this.description = Objects.requireNonNull(description);
+        this.weight = Objects.requireNonNull(weight);
+    }
+
+    public static void main(String[] args){
+        final Product product= new Product(12L, null, "descriere", 77L);
+    }
 }
